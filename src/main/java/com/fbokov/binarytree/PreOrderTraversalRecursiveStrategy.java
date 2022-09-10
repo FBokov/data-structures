e8267019d9d598c819c67ebcff6d15fa76d7ffea
philipp.bokov <fbokov@gmail.com>
2022-09-10 15:15:42 +0300

    pre order recurvive

--- src/main/java/com/fbokov/binarytree/PreOrderTraversalRecursiveStrategy.java
+++ src/main/java/com/fbokov/binarytree/PreOrderTraversalRecursiveStrategy.java
@@ -1,11 +1,23 @@
 package com.fbokov.binarytree;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PreOrderTraversalRecursiveStrategy implements TraversalStrategy {
     @Override
     public List<Integer> traversal(TreeNode root) {
-        // TODO Auto-generated method stub
-        return null;
+        if (root == null) return List.of();
+        
+        List<Integer> preOrder = new ArrayList<>();
+        preOrder.add(root.val);
+
+        if (root.left != null) {
+            preOrder.addAll(traversal(root.left));
+        } 
+
+        if (root.right != null) {
+            preOrder.addAll(traversal(root.right));
+        }
+        return preOrder;
     }
 }
